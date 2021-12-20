442faf22360d9bb98796fd72832491c883a2c10e
Logan V <logan2211@gmail.com>
2021-12-19 18:39:38 -0600

    Switch sshkey command to table output

--- src/commands/GetProjectCommand.php
+++ src/commands/GetProjectCommand.php
@@ -2,6 +2,7 @@
 
 namespace Limestone\Command;
 
+use Limestone\SDK\Client;
 use Limestone\SDK\Model\V2ProjectPostBody;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,29 +10,41 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class GetProjectCommand extends AbstractCommand
+class GetProjectCommand extends AbstractGetCommand
 {
     use \Limestone\InteractsWithApi;
 
     protected static $defaultName = 'project:get';
+    protected ?string $command_description = 'Get information about a project';
+
+    protected array $supported_output = ['table', 'json'];
+    protected bool $horizontal_table = true;
 
     protected function configure()
     {
         parent::configure();
 
-        $this
-            ->setDescription('Get a project.')
-            ->addOption('project','',InputOption::VALUE_REQUIRED,'The project id')
-            // the full command description shown when running the command with
-            // the "--help" option
-            ->setHelp('This command allows you to get a project...');
+        $this->addArgument('project', InputArgument::REQUIRED, 'Project ID');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function getResult(InputInterface $input, Client $client)
     {
-        $client = $this->getClient();
-        $result = $client->getProject($input->getOption('project'));
-        $output->write($this->toJson($result),true);
-        return parent::SUCCESS;
+        return $client->getProject($input->getArgument('project'));
+    }
+
+    protected function getTableHeader(): ?array
+    {
+        return ['UUID', 'Project ID', 'Name'];
+    }
+
+    protected function getTableRows($data): ?array
+    {
+        $return = [
+            $data->getUuid(),
+            $data->getProjectId(),
+            $data->getName(),
+        ];
+
+        return [$return];
     }
 }
--- src/commands/GetSSHKeyCommand.php
+++ src/commands/GetSSHKeyCommand.php
@@ -2,33 +2,48 @@
 
 namespace Limestone\Command;
 
+use Limestone\SDK\Client;
 use Limestone\SDK\Model\V2ProjectPostBody;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class GetSSHKeyCommand extends AbstractCommand
+class GetSSHKeyCommand extends AbstractGetCommand
 {
     use \Limestone\InteractsWithApi;
 
     protected static $defaultName = 'sshkey:get';
+    protected ?string $command_description = 'Get an SSH key';
+
+    protected array $supported_output = ['table', 'json'];
+    protected bool $horizontal_table = true;
 
     protected function configure()
     {
         parent::configure();
 
-        $this
-            ->setDescription('Get an ssh key')
-            ->addArgument('name',InputArgument::REQUIRED,'The ssh key name')
-            ->setHelp('This command allows you to get an ssh key...');
+        $this->addArgument('name', InputArgument::REQUIRED, 'SSH Key Name');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function getResult(InputInterface $input, Client $client)
     {
-        $client = $this->getClient();
-        $result = $client->getSSHKey($input->getArgument("name"));
-        $output->write($this->toJson($result), true);
-        return parent::SUCCESS;
+        return $client->getSSHKey($input->getArgument('name'));
+    }
+
+    protected function getTableHeader(): ?array
+    {
+        return ['ID', 'Name', 'Fingerprint', 'Comment'];
+    }
+
+    protected function getTableRows($data): ?array
+    {
+        $return = [
+            $data->getId(),
+            $data->getName(),
+            $data->getFingerprint(),
+            $data->getComment(),
+        ];
+        return [$return];
     }
 }
